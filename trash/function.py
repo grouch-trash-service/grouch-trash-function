1feae52b5fa0d77cdb94e8fd852a95bf078cc5d8
Matt Porter <matthew.js.porter@gmail.com>
2020-09-30 21:44:54 -0400

    adding config.ini for configuration and moving logging config to this file

--- trash/function.py
+++ trash/function.py
@@ -3,13 +3,15 @@ lambda function for getting which day of the week trash pickup is
 """
 import logging
 import logging.config
+import configparser
 import trash_day as trash
 from schedule import TrashScheduleService
 
-trash_schedule_service = TrashScheduleService(
-    'https://dp8mqqk471.execute-api.us-east-1.amazonaws.com/Prod/v1/holidays')
+config = configparser.ConfigParser()
+config.read('config.ini')
+trash_schedule_service = TrashScheduleService(config['DEFAULT']['TrashScheduleServiceUrl'])
 
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig('config.ini')
 
 
 def lambda_handler(event, context) -> dict:
